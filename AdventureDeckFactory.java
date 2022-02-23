c2a093735b085abcffbd141f23bcd488a14e727a
HenryZhangxiao <44578113+HenryZhangxiao@users.noreply.github.com>
2022-02-23 17:25:41 -0500

    Prelim deck creation for the Adventure Deck

---
+++ AdventureDeckFactory.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public interface AdventureDeckFactory {
+    Card drawCard();
+    void discardCard(Card c);
+    void reshuffle(ArrayList<Card> arr);
+    void initializeCards();
+}
--- Game.java
+++
@@ -1,8 +0,0 @@
-import java.util.ArrayList;
-
-public class Game {
-    public static void main (String[] args){
-        ArrayList<WeaponCard> weaponDeck = new ArrayList<>();
-
-    }
-}
