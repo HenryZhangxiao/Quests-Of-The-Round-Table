89f5adf776304fbb38778423ac6756038822a136
AlexMuir65 <55604861+AlexMuir65@users.noreply.github.com>
2022-03-01 14:43:26 -0500

    Player functionality now in terms of cardIDs

--- src/main/java/org/example/Player.java
+++ src/main/java/org/example/Player.java
@@ -7,17 +7,72 @@ public class Player{
     private int playerNum;
     private String playerName;
     public ArrayList<Card> hand;
-    //public ArrayList<Card> board;  //amours, weapons, etc in front of them
 
     public Player(int ID, String name){
         playerName = name;
         playerNum = ID;
         hand = new ArrayList<>();
-        //board = new ArrayList<>();
     }
 
     public void addCard(Card c){
         hand.add(c);
+    }
+
+    protected Card getCardByID(int id){ //TODO will have to change to follow order of single Image with all cards
+        switch(id){
+            case 1:
+                return new ExcaliburWeapon();
+            //break;
+            case 2:
+                return new LanceWeapon();
+            //break;
+            case 3:
+                return new BattleAxWeapon();
+            //break;
+            case 4:
+                return new SwordWeapon();
+            //break;
+            case 5:
+                return new HorseWeapon();
+            //break;
+            case 6:
+                return new DaggerWeapon();
+            //break;
+            case 7:
+                return new DragonFoe();
+            //break;
+            case 8:
+                return new GiantFoe();
+            //break;
+            case 9:
+                return new MordredFoe();
+            //break;
+            case 10:
+                return new GreenKnightFoe();
+            //break;
+            case 11:
+                return new BlackKnightFoe();
+            //break;
+            case 12:
+                return new EvilKnightFoe();
+            //break;
+            case 13:
+                return new SaxonKnightFoe();
+            //break;
+            case 14:
+                return new RobberKnightFoe();
+            //break;
+            case 15:
+                return new SaxonsFoe();
+            //break;
+            case 16:
+                return new BoarFoe();
+            //break;
+            case 17:
+                return new ThievesFoe();
+            //break;
+        }
+        return new ThievesFoe(); //Should never be reached
     }
 
     public int[] getHandCardIDs(){
@@ -29,26 +84,25 @@ public class Player{
     }
 
     public void addCardsByIDs(int[] cardIDs){
-        //todo
+        for(int i = 0; i < cardIDs.length; ++i){
+            addCardByID(cardIDs[i]);
+        }
     }
 
     public void addCardByID(int cardID){
-        //todo need a way to lookup a card by ID and get back a card object before adding it to the hand.
+        hand.add(getCardByID(cardID));
     }
 
     protected void discardCardFromHand(int cardID){
-        //Card card = hand.get(index);
-        //hand.remove(card);
-        //card.discard();
-
-        //todo this needs to be by cardID, not by index in the hand.
+        Card card = getCardByID(cardID);
+        hand.remove(card);
+        card.discard();
     }
 
-    protected void discardCardsFromHand(Integer[] indices){
-        //for(int i = 0; i < indices.length; ++i){
-        //    discardCardFromHand(indices[i]);
-        //}
-        //todo same as above
+    protected void discardCardsFromHand(Integer[] cardIDs){
+        for(int i = 0; i < cardIDs.length; ++i){
+            discardCardFromHand(cardIDs[i]);
+        }
     }
 
     public void setPlayerNum(int playerNum) {
